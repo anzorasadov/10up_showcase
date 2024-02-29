c6d8d5d2935dddb5fdd1b3674435c398036c3984
Anzor <anzor@layered-studio.com>
2024-02-29 16:05:38 +0100

    feat: extend index page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,19 @@
 import Link from "next/link";
-import LinkPreview from "./components/LinkPreview/LinkPreview";
 import { Metadata } from "next";
+import { promises as fs } from "fs";
+import LinkPreview from "./components/LinkPreview/LinkPreview";
+import CodeBlock from "./components/CodeBlock/CodeBlock";
 
 export const metadata: Metadata = {
   title: "10up Technical Submission",
   description: "Collection of code examples for 10up recruitment process",
 };
 
-export default function Home() {
+export default async function Home() {
+  const crowddeskService = await fs.readFile(process.cwd() + "/public/code/CrowddeskService.txt", "utf8");
+  const crowddeskAdapter = await fs.readFile(process.cwd() + "/public/code/CrowddeskAdapter.txt", "utf8");
+  const elementorProjectsOverview = await fs.readFile(process.cwd() + "/public/code/ElementorProjectsOverview.txt", "utf8");
+  const elementorProjectSingle = await fs.readFile(process.cwd() + "/public/code/ElementorProjectSingle.txt", "utf8");
   return (
     <main className="container">
       <section>
@@ -46,8 +52,10 @@ export default function Home() {
       <section id="semantic-html">
         <h2>Semantic HTML</h2>
         <p>
-          This demo webpage was created with semantic HTML in mind consisting of <code>{"<header />"}</code>, <code>{"<nav />"}</code>,{" "}
-          <code>{"<main />"}</code>, <code>{"<section />"}</code>s, <code>{"<figures />"}</code>s and <code>{"<article />"}</code>s.
+          This demo webpage was created with semantic HTML in mind consisting of <code className="inline">{"<header />"}</code>,{" "}
+          <code className="inline">{"<nav />"}</code>, <code className="inline">{"<main />"}</code>,{" "}
+          <code className="inline">{"<section />"}</code>s, <code className="inline">{"<figures />"}</code>s and{" "}
+          <code className="inline">{"<article />"}</code>s.
         </p>
         <p>
           The source code for this demo can be viewed and downloaded from{" "}
@@ -73,8 +81,10 @@ export default function Home() {
             <LinkPreview url="https://www.saniadmina.com/" target="new" />
             <p className="px-1">
               Base theme: Storefront <br />
-              Around 50% custom CSS <br />
-              CSS grid by Bootstrap
+              CSS grid by Bootstrap <br />
+              <a href="https://www.saniadmina.com/wp-content/themes/storefront-child-theme-master/style.css?ver=1.0.0" target="new">
+                Around 50% custom CSS
+              </a>
             </p>
           </div>
 
@@ -82,8 +92,10 @@ export default function Home() {
             <LinkPreview url="https://birnleitner-immobilien.at/" target="new" />
             <p className="px-1">
               Base theme: MyHome <br />
-              Around 60% custom CSS <br />
-              No CSS frameworks
+              No CSS frameworks <br />
+              <a href="https://birnleitner-immobilien.at/wp-content/themes/myhome-child/style.css?ver=3.1.66" target="new">
+                Around 50% custom CSS
+              </a>
             </p>
           </div>
 
@@ -91,7 +103,7 @@ export default function Home() {
             <LinkPreview url="https://layered-2023.vercel.app/en" target="new" contain />
             <p className="px-1">
               [WIP] Own website <br />
-              Custom CSS based on Tailwind CSS
+              <a href="https://github.com/anzorasadov/layered-2023">Custom CSS based on Tailwind CSS (global.css and components)</a>
             </p>
           </div>
         </div>
@@ -103,7 +115,7 @@ export default function Home() {
           While Design is not my area of expertise, I do hope I could demonstrate my rudementary design skills well enough with this demo
         </p>
         <p>Further references:</p>
-        <p className="three-col py-1">
+        <div className="three-col py-1">
           <div>
             <LinkPreview url="https://www.saniadmina.com/" target="new" />
             <p className="px-1">Styling in collaboration with the Client</p>
@@ -128,23 +140,57 @@ export default function Home() {
             <LinkPreview url="https://gulz.at/" target="new" />
             <p className="px-1">Styling in collaboration with the my business partner</p>
           </div>
-        </p>
+        </div>
       </section>
 
       <section id="javascript">
         <h2>JavaScript</h2>
-        <p>...</p>
+        <p>Here is a summary of a few JS building blocks that I have setup for clients</p>
       </section>
 
       <section id="react">
         <h2>React</h2>
-        <p>...</p>
+        <p>
+          Most of my React Code has been produced as a part of proprietary software that I cannot show due to NDAs that I've signed.
+          However, this page was set up in Next.js, which is based on React. Furthermore, the WIP website{" "}
+          <a href="https://layered-2023.vercel.app/en" target="new">
+            Layered Studio
+          </a>{" "}
+          <a href="https://github.com/anzorasadov/layered-2023" target="new">
+            (repo)
+          </a>{" "}
+          is also built with Next.js. Although these Demos are rudementary, I do hope that they could provide some insight.
+        </p>
       </section>
 
       <section id="wordpress">
         <h2>WordPress / PHP</h2>
-        <p>...</p>
-        <p></p>
+        <p>
+          Usually I am requested by my clients to implement some custom functions or connect a WP-Website to an API. As demonstrated in the
+          following example, I have connected a Crowdfunding API to a clients website in order to display their current Crowdfunding
+          Projects on thier website.
+        </p>
+
+        <CodeBlock className="my-1" isCollapsable title="Crowddesk Serice API">
+          {crowddeskService}
+        </CodeBlock>
+
+        <CodeBlock className="my-1" isCollapsable title="Crowddesk Data Adapter">
+          {crowddeskAdapter}
+        </CodeBlock>
+
+        <p>
+          To allow this I had to write a custom building block for Elementor to give the user a toolbox for displaying and curating the
+          contents.
+        </p>
+
+        <CodeBlock className="my-1" isCollapsable title="Elementor widget for displaying projects lists from Crowddesk">
+          {elementorProjectsOverview}
+        </CodeBlock>
+
+        <CodeBlock className="my-1" isCollapsable title="Elementor widget for displaying single Project from Crowddesk">
+          {elementorProjectSingle}
+        </CodeBlock>
       </section>
     </main>
   );
